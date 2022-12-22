9ff49f422ae15267ddac50517f525155fcd309bb
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-12-22 12:26:54 +0200

    feat: implement basic chat interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useEffect, useState, KeyboardEvent} from 'react';
 import './App.css';
+import {io} from 'socket.io-client';
+
+type Message = {
+    id: string
+    message: string
+    user: {
+        id: string,
+        name: string
+    }
+}
+
+// const socket = io('https://chat-socket-io-back-production.up.railway.app/');
+const socket = io('http://localhost:5000/');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [text, setText] = useState('')
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const onchangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.currentTarget.value)
+    }
+
+    const onClickHandler = () => {
+        socket.emit('client-message-sent', text)
+        setText('')
+    }
+
+    const onEnterDownHandler = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        e.key === 'Enter' && onClickHandler()
+    }
+
+    useEffect(() => {
+        socket.on('init-messages-published', (messages: Message[]) => {
+            setMessages(messages)
+        })
+        socket.on('new-message-sent', (newMessage: Message) => {
+            setMessages((prevMessages) => [...prevMessages, newMessage])
+        })
+    }, [])
+
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <div className="messages">
+                    {messages.map(m => <div key={m.id}>
+                        <b>{m.user.name}: </b> {m.message}
+                        <hr/>
+                    </div>)}
+                </div>
+                <div className="textarea">
+                    <textarea value={text} onChange={onchangeHandler} onKeyDown={onEnterDownHandler}/>
+                    <button onClick={onClickHandler}>Send</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
